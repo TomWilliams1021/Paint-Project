36817ad121a68ec9ad9713ca9fed3bf3c283f24f
TomWilliams1021 <tomwilliams1021@gmail.com>
2022-01-20 14:23:34 +0000

    First Commit, initial version with hard coded solution

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Random randomNumber = new Random();
+        short lowRand = 1000;
+        short highRand = 20000;
+
+        short numOfCoatsOfPaint = 2;
+        short paintCoveragePerLitre = 6;
+        short priceOfPaintPerLitreInPounds = 5;
+        short squareMetersPaintedPerHour = 7;
+        int areaOfWallInMSquared = 0;
+        int priceEstimate = 0;
+        int volumeOfPaintNeededInLitres = 0;
+        int wallAreaDividePaintCoverage = 0;
+        int numOfWalls = randomNumber.nextInt(5-1) + 1;
+        int totalCost = 0;
+        int totalPaintInLitres = 0;
+        //WIP
+        int totalManHours = 0;
+        int labourCostPerHour = 10;
+        int totalCostOfLabour = 0;
+
+
+        System.out.println("There are " + numOfWalls +" walls to paint.");
+
+        for(int loopCounter = 1; loopCounter <= numOfWalls; loopCounter++) {
+
+            int heightOfWallInMM = randomNumber.nextInt(highRand-lowRand) + lowRand;
+            int widthOfWallInMM = randomNumber.nextInt(highRand-lowRand) + lowRand;
+
+            areaOfWallInMSquared = (heightOfWallInMM / 1000) * (widthOfWallInMM / 1000);
+            wallAreaDividePaintCoverage = (int) Math.ceil(areaOfWallInMSquared / paintCoveragePerLitre);
+            volumeOfPaintNeededInLitres = (wallAreaDividePaintCoverage * numOfCoatsOfPaint);
+            priceEstimate = volumeOfPaintNeededInLitres * priceOfPaintPerLitreInPounds;
+
+            System.out.println("The area of this wall is " + areaOfWallInMSquared + " square meters. Therefore you will need at least " + volumeOfPaintNeededInLitres + " litres of paint.");
+            System.out.println("This will cost £" + priceEstimate + ".");
+
+            totalManHours += (int) Math.ceil(areaOfWallInMSquared / squareMetersPaintedPerHour);
+            totalPaintInLitres += volumeOfPaintNeededInLitres;
+            totalCost += priceEstimate;
+        }
+
+        totalCostOfLabour = totalManHours * labourCostPerHour;
+
+        System.out.println("You will need " + totalPaintInLitres + " Litres of paint.");
+        System.out.println("The total cost of paint for this project will be roughly £" + totalCost + ".");
+        System.out.println("The total labour cost for this project will be roughly £" + totalCostOfLabour + " at a rate of £" + labourCostPerHour + " per-hour.");
+
+    }
+}
